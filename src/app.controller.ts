3379b0e64dd36744d835506663ef8f88b8e8b7ad
Sstandby <standbyy9@gmail.com>
2022-04-10 17:53:46 -0500

    NestJS passaport-jwt

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,13 +1,21 @@
-import { Controller, Post, Request, UseGuards } from '@nestjs/common';
+import { Controller, Post, Get, Request, UseGuards } from '@nestjs/common';
 import { LocalAuthGuard } from './auth/local-auth.guard';
-
+import { JwtAuthGuard } from './auth/jwt-auth.guard';
+import { AuthService } from './auth/auth.service';
 @Controller()
 export class AppController {
   //UseGuards es un Guardian para validar que una solicitud sea manejada por un controlador
+  constructor(private authService: AuthService) {}
 
   @UseGuards(LocalAuthGuard)
   @Post('auth/login')
   async login(@Request() req) {
+    return this.authService.login(req.user);
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Get('profile')
+  getProfile(@Request() req) {
     return req.user;
   }
 }
---
+++ src/auth/constants.ts
@@ -0,0 +1,5 @@
+//Esto es un claro ejemplo, NO usarse en un entorno real, para eso se sugiere dejar la clave en una variable de entorno o bóveda de secretos.
+
+export const jwtConstants = {
+  secret: 'secretKey',
+};
