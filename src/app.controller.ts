7997a5eea19962c89c5946c6a74909db6389aed9
Sstandby <standbyy9@gmail.com>
2022-04-10 20:24:47 -0500

    NestJS passaport-jwt

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,11 +1,27 @@
-import { Controller, Post, Get, Request, UseGuards } from '@nestjs/common';
+import {
+  Controller,
+  Param,
+  Delete,
+  Patch,
+  Body,
+  Post,
+  Get,
+  Request,
+  UseGuards,
+  ParseIntPipe,
+} from '@nestjs/common';
 import { LocalAuthGuard } from './auth/local-auth.guard';
 import { JwtAuthGuard } from './auth/jwt-auth.guard';
 import { AuthService } from './auth/auth.service';
+import { UsersService } from './users/users.service';
+
 @Controller()
 export class AppController {
   //UseGuards es un Guardian para validar que una solicitud sea manejada por un controlador
-  constructor(private authService: AuthService) {}
+  constructor(
+    private authService: AuthService,
+    private usersService: UsersService,
+  ) {}
 
   @UseGuards(LocalAuthGuard)
   @Post('auth/login')
@@ -17,5 +33,25 @@ export class AppController {
   @Get('profile')
   getProfile(@Request() req) {
     return req.user;
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Patch('crear_users')
+  createUser(@Body() body) {
+    // return body;
+    // return `Crear un jugador: ${body.name} y juega de ${body.position}`;
+    return this.usersService.crear_usuario(body);
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Get('search/:id')
+  getSearch(@Param('id', ParseIntPipe) id) {
+    return this.usersService.consultar_id(id);
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Delete('eliminar/:id')
+  getEliminar(@Param('id', ParseIntPipe) id) {
+    return this.usersService.eliminar_id(id);
   }
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -17,8 +17,27 @@ export class UsersService {
     },
   ];
 
+  // Buscar usuario por username
   async consultar_usuario(username: string): Promise<User | undefined> {
-    return this.users.find(User => User.username === username);
+    return this.users.find((user) => user.username === username);
   }
 
+  // Buscar usuario por id
+  async consultar_id(id: any): Promise<User | undefined> {
+    return this.users.find((user) => user.id === id);
+  }
+
+  // Eliminar usuario por id
+  async eliminar_id(id: any): Promise<User[] | undefined> {
+    return this.users.filter((user) => user.id !== id);
+  }
+
+  // crear usuario
+  async crear_usuario(user): Promise<User | undefined> {
+    this.users.push({
+      id: this.users.length + 1,
+      ...user,
+    });
+    return this.users[this.users.length - 1];
+  }
 }
